2ecf61a20c7bbf078f7fc1432df51523aae2015b
LeonNapoleonov <127660649+LeonNapoleonov@users.noreply.github.com>
2023-06-01 17:13:38 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -2,35 +2,34 @@
 # python3
 
 def read_input():
-    return (input().rstrip(), input().rstrip())
-
+    fileorno = input()
+    if "I" in fileorno or "i" in fileorno:
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif "F" in fileorno or "f" in fileorno:
+        file = "06"
+        if "a" not in file:
+            with open("tests/" + file, 'r')as f:
+                pattern = f.readline().rstrip()
+                text = f.readline().rstrip()
+    return pattern, text
 def print_occurrences(output):
     print(' '.join(map(str, output)))
-
 def get_occurrences(pattern, text):
+    prime = 1
+    p_len, t_len = len(pattern), len(text)
+    pattern_hash = sum(ord(pattern[i]) * pow(prime, i) for i in range(p_len))
+    text_hash = sum(ord(text[i]) * pow(prime, i) for i in range(p_len))
     occurrences = []
-    pattern_len = len(pattern)
-    text_len = len(text)
-    prime = 998244353
-    prime_power = 1
-    for _ in range(pattern_len - 1):
-        prime_power = (prime_power * 26) % prime
-
-    pattern_hash = 0
-    text_hash = 0
-    for i in range(pattern_len):
-        pattern_hash = (pattern_hash * 26 + ord(pattern[i]) - ord('a')) % prime
-        text_hash = (text_hash * 26 + ord(text[i]) - ord('a')) % prime
-
-    for i in range(text_len - pattern_len + 1):
-        if pattern_hash == text_hash and pattern == text[i:i+pattern_len]:
-            occurrences.append(i)
-
-        if i < text_len - pattern_len:
-            text_hash = (text_hash - (ord(text[i]) - ord('a')) * prime_power) % prime
-
-            if text_hash < 0:
-                text_hash += prime
+    for i in range(t_len - p_len + 1):
+        if pattern_hash == text_hash:
+            if pattern == text[i:i+p_len]:
+                occurrences.append(i)
+        if i < t_len - p_len:
+            text_hash = (text_hash - ord(text[i])) / prime + ord(text[i+p_len]) * pow(prime, p_len-1)
+    return occurrences
+if __name__ == '__main__':
+    print_occurrences(get_occurrences(*read_input()))
 
             text_hash = (text_hash * 26 + ord(text[i+pattern_len]) - ord('a')) % prime
     return occurrences
